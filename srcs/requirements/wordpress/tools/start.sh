09bcecea0829f7fb60128e59e43f217febc52294
Hyeon Na <hyna@c5r2s4.42seoul.kr>
2023-01-31 17:10:53 +0900

    feat : 서비스들 pid 1로 실행하도록 구현

--- srcs/requirements/wordpress/tools/start.sh
+++ srcs/requirements/wordpress/tools/start.sh
@@ -1,2 +1,15 @@
-service php7.3-fpm start
-bash
+chown -R www-data:www-data /var/www/
+
+if [ ! -f "/var/www/html/index.php" ]; then
+    set -x
+	sudo -u www-data sh -c " \
+    wp core download --locale=ko_KR && \
+    wp config create --dbname=$MYSQL_DATABASE --dbhost=$MYSQL_HOSTNAME \
+    --dbuser=$MYSQL_USER --dbpass=$MYSQL_USER_PASSWORD && \
+    wp core install --url=$DOMAIN_NAME --title=$WP_TITLE \
+    --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PW --admin_email=$WP_ADMIN_EMAIL && \
+    wp user create $WP_USER $WP_USER_EMAIL --role=author --user_pass=$WP_PASS \
+	"
+fi
+
+exec /usr/sbin/php-fpm7.3 -F
