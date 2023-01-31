582e8043a8758393964022d6816fcb2dc1bad578
Hyeon Na <hyna@c5r2s4.42seoul.kr>
2023-01-31 15:13:26 +0900

    fix : mysql비밀번호 변경함

--- srcs/requirements/mariadb/tools/initial_db.sql
+++ srcs/requirements/mariadb/tools/initial_db.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE IF NOT EXISTS wordpress_db;
-CREATE USER IF NOT EXISTS 'hyna'@'%' IDENTIFIED BY 'testing_';
+CREATE USER IF NOT EXISTS 'hyna'@'%' IDENTIFIED BY 'psswd1234_';
 GRANT ALL PRIVILEGES ON wordpress_db.* TO 'hyna'@'%';
 FLUSH PRIVILEGES;
 
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'testing_';
+ALTER USER 'root'@'localhost' IDENTIFIED BY 'psswd1234_';
