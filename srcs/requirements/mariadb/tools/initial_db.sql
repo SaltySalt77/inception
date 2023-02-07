641306887f143e93aae9aa6cc3703117b51d9b44
Hyeon Na <hyna@c3r2s7.42seoul.kr>
2023-02-07 11:24:29 +0900

    refact : debian-buster 대신 11.5 받도록 함

--- srcs/requirements/mariadb/tools/initial_db.sql
+++
@@ -1,6 +0,0 @@
-CREATE DATABASE IF NOT EXISTS wordpress_db;
-CREATE USER IF NOT EXISTS 'hyna'@'%' IDENTIFIED BY 'psswd1234_';
-GRANT ALL PRIVILEGES ON wordpress_db.* TO 'hyna'@'%';
-FLUSH PRIVILEGES;
-
-ALTER USER 'root'@'localhost' IDENTIFIED BY 'psswd1234_';
